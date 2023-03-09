596f74fe6b7c8f7cc7c927d85c1c8b42648e6a1a
yuval1847 <117625084+yuval1847@users.noreply.github.com>
2023-03-09 16:52:53 +0200

    Add files via upload

---
+++ prog2_p59_7/prog2_p59_7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace prog2_p59_7
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int min;
+            Console.WriteLine("Enter minutes:");
+            min = int.Parse(Console.ReadLine());
+            Console.WriteLine("The time is: "+(min/60)+" H "+(min%60)+" min");
+        }
+    }
+}
---
+++ prog2_p59_8/prog2_p59_8/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace prog2_p59_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("Enter the childern number:");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("there are:"+(x/2)+" couples");
+            Console.WriteLine("there are:" + (x % 2) + "children that don't have couple");
+            Console.WriteLine("there are:" + (x % 3) + "children that don't have triple");
+        }
+    }
+}
