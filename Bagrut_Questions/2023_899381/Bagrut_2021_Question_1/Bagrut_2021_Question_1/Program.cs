5313fc3c37a8bbeb9f82c36b3235c2912a797a2b
yuval1847 <117625084+yuval1847@users.noreply.github.com>
2023-10-05 11:58:13 +0300

    Add files via upload

---
+++ Bagrut_Questions/2023_899381/Bagrut_2021_Question_1/Bagrut_2021_Question_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.InteropServices;
+
+internal class Program
+{
+    public static int[] Filter(int[] arr, int num)
+    {
+        // The function get an array of integers and an integer
+        // The function return a new integers array which contain only the values from 
+        // the given array which aren't equal to num
+        int lengthWithoutNum = 0, newArrayIndex = 0;
+        for(int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] != num)
+            {
+                lengthWithoutNum++;
+            }
+        }
+        int[] newArray = new int[lengthWithoutNum];
+        for(int i = 0; i < arr.Length; i++)
+        {
+            if(arr[i] != num)
+            {
+                newArray[newArrayIndex] = arr[i];
+                newArrayIndex++;
+            }
+        }
+        return newArray;
+    }
+
+    private static void Main(string[] args)
+    {
+        int[] array = { 1, 2, 3, 4, 3, 5, 6 };
+        int[] newArray = Filter(array, 3);
+        for(int i = 0; i < newArray.Length; i++)
+        {
+            Console.WriteLine(newArray[i]);
+        }
+    }
+}
