c9ba54451d2b5c0142f52b83e464fe97ce8bc0d3
yuval1847 <117625084+yuval1847@users.noreply.github.com>
2024-09-11 14:49:40 +0300

    Add files via upload

---
+++ Prog_1_winforms/Prog_1_winforms/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Prog_1_winforms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtData
+            // 
+            this.txtData.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtData.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.txtData.ForeColor = System.Drawing.Color.Gold;
+            this.txtData.Location = new System.Drawing.Point(355, 271);
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(492, 44);
+            this.txtData.TabIndex = 1;
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Font = new System.Drawing.Font("Impact", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.ForeColor = System.Drawing.Color.OrangeRed;
+            this.lblInfo.Location = new System.Drawing.Point(440, 84);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(332, 44);
+            this.lblInfo.TabIndex = 3;
+            this.lblInfo.Text = "Welcome To My Lable";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ClientSize = new System.Drawing.Size(1148, 717);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.txtData);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
+
---
+++ Prog_1_winforms/Prog_1_winforms/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Prog_1_winforms
+{
+    public partial class Form1 : Form
+    {
+        
+        private string IpAddress; 
+        private TcpListener tcpLsn; 
+        private Thread tcpThd;
+        ClientConnect c1;
+        List<ClientConnect> allClients = new List<ClientConnect>();
+        private static int connectId = 0; // משתנה ששומר את מספר הלקוחות שהתחברו ואת מספר הלקוח 
+        string NameOfUsers, str;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void GetIpAdDress() // את כתובת המחשב IpAddress פונקציה שתשמור במשתנה IpAdress את הכתובת
+        {
+            IPAddress[] localIP = Dns.GetHostAddresses(Dns.GetHostName());
+            IpAddress = Convert.ToString(localIP[localIP.Length - 1]);
+            // IpAddress ="127.0.0.1";        
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetIpAdDress();
+            tcpLsn = new TcpListener(System.Net.IPAddress.Parse(IpAddress), 8002);//  יצירת מאזין- 
+            tcpLsn.Start();// מפעילה את המאזין
+            lblInfo.Text = "Listen at: " + tcpLsn.LocalEndpoint.ToString();// מציג את הכתובת והפורט
+            c1 = new ClientConnect();
+            tcpThd = new Thread(new ThreadStart(NewClientConnected));// יצירת תהליך שפועל ברקע   
+            tcpThd.Start();
+        }
+
+
+        public void NewClientConnected() //זו פונקציה שאנו נרשום אשר פעולותיה מתבצעות ברקע של התוכנית 
+        {
+            //פונקציה המפעילה תהליך חדש
+            //היא בעלת לולאה אינסופית(זהו תהליך ותהליך לא נגמר אף פעם) המטפלת בהתחברות של הלקוח החדש
+            Socket s;
+            //s = tcpLsn.AcceptSocket();
+            string strmess;
+
+            while (true)
+            {
+                try
+                {
+                    s = tcpLsn.AcceptSocket();
+                    c1 = new ClientConnect();
+                    c1.clientSocket = s;
+                    c1.clientThread = new Thread(new ThreadStart(ReadSocket)); //Threadשומרת את ה
+                    int ret = 0;
+                    Byte[] receive = new Byte[10]; //ברשת נתונים עוברים רק בביטים
+                    ret = s.Receive(receive, receive.Length, 0); //מחזיק את כמות הבייטים שיתקבלו
+                    strmess = System.Text.Encoding.UTF8.GetString(receive); //strmess שורה זו ממירה את המערך שהוא מטיפוס בייט למחרוזת שאותה הוא שומר ב
+                    c1.name = strmess.Substring(0, ret); //שורה זו מחלצת את מה שרשום עד לרווח ה1
+                    Interlocked.Increment(ref connectId);
+                    c1.clientnum = connectId;
+                    UpDateDataGrid(connectId + " : " + strmess.Substring(0, ret) + "\r\n");
+
+                    lock (this)
+                    {
+                        allClients.Add(c1); //את שעת ההתחברות textboxמעדכנת את הטבלה ומוסיפה ל 
+                        UpDateDataGrid(" Connected> " + connectId + " " + DateTime.Now.ToLongTimeString() + "\r\n");
+                        c1.clientThread.Start();
+                    }
+                }
+                catch (Exception e)
+                {
+                    break;
+                }
+            }
+        }
+
+
+
+        public void UpDateDataGrid(string displayString)//אם תוך כדי תהליכים נצטרך לעדכן תיבת טקסט או כפתורים על הטופס נעשה אינבואוק
+        {
+            if (txtData.InvokeRequired)
+                txtData.Invoke(new MethodInvoker(() => txtData.AppendText(displayString + "\n")));
+
+
+        }
+
+        public void ReadSocket()// תהליך של כול לקוח
+        {
+
+            long realId = c1.clientnum; // The realId saves the real number of the client that sends the info
+            Socket s = c1.clientSocket;
+            int ret = 0; // This object will contain the number of characters that are passed in the message
+            Byte[] receive; // In this array I'll save the info from the client
+            receive = new Byte[2000]; // If the client is connected we reboot the array;
+            while (true) // מנהל המשחק: כרגע מקבלת ממל מי כול לקוח ומוסרת אותו לכול הלקוחות
+            {
+                try
+                {
+                    if (s.Connected)
+                    {
+                        ret = s.Receive(receive, receive.Length, 0);//s.Receive is a command that gets the info from the client and put it in the array receive
+                        if (ret > 0) // If a message is rececived
+                        {
+                            foreach (ClientConnect c in allClients)
+                            {
+                                if (c.clientSocket.Connected)
+                                    c.clientSocket.Send(receive, receive.Length, SocketFlags.None);
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }// If a message was received and its' characters length is 0 we get out of the loop
+                }
+                catch (Exception e) // If an error occured we want to stop the thread
+                {
+                    UpDateDataGrid(e.ToString()); // Show on the screen in the txtbox the error that occured
+                    if (!s.Connected) break;// If the client is not connected we get out of the loop
+                }
+            }
+
+
+            CloseTheThread(realId);// We'll get to this line only if an error occured, because we only get out of the loop if there was an error
+                                   //that's why this function will only be summaned if an error occured
+        }
+        private void CloseTheThread(long realId)
+        {
+            //This function closes the thread - the process of the client that caused the error
+            try
+            {
+                for (int i = 0; i < allClients.Count; i++)
+                {
+                    if (allClients[i].clientnum == realId)
+                        allClients[i].clientThread.Abort();
+                }
+            }
+
+            catch (Exception e)
+            {
+                lock (this)
+                {
+                    for (int i = 0; i < allClients.Count; i++)
+                    {
+                        if (allClients[i].clientnum == realId)
+                            allClients.Remove(allClients[i]);
+                    }
+
+                    UpDateDataGrid("Disconnected>" + realId + " " + DateTime.Now.ToLongTimeString());
+                }
+            }
+        }
+        private void OnClosing() // זוהי פונקציה שמחסלת תהליכים היא סוגרת את הthread ואת המאזין
+        {
+            if (tcpLsn != null)
+            { tcpLsn.Stop(); }
+
+            foreach (ClientConnect cd in allClients)
+            {
+                if (cd.clientSocket.Connected) cd.clientSocket.Close();
+                if (cd.clientThread.IsAlive) cd.clientThread.Abort();
+            }
+
+
+            if (tcpThd.IsAlive) tcpThd.Abort();
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            OnClosing();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            OnClosing();
+        }
+
+    }
+}
