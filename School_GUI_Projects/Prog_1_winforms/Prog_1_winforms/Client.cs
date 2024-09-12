87a21d87d3f1e784eaf0e9dd8c692c303f897bc6
yuval1847 <117625084+yuval1847@users.noreply.github.com>
2024-09-12 14:46:24 +0300

    Add files via upload

---
+++ School_GUI_Projects/Prog_1_winforms/Prog_1_winforms/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Prog_1_winforms
+{
+    class ClientConnect
+    {
+        public int clientnum;
+        public string name;  //שם הלקוח שיתחבר לשרת 
+        public Socket clientSocket; // הסוקט שדרכו הלקוח התחבר לשרת 
+        public Thread clientThread;//התהליך של הלקוח שדרכו תתנהל כל ההתקשרות של השרת מול הלקוח 
+    }
+
+}
---
+++ School_GUI_Projects/Prog_1_winforms/Prog_1_winforms/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Prog_1_winforms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtData
+            // 
+            this.txtData.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtData.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.txtData.ForeColor = System.Drawing.Color.Gold;
+            this.txtData.Location = new System.Drawing.Point(237, 176);
+            this.txtData.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(329, 32);
+            this.txtData.TabIndex = 1;
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Font = new System.Drawing.Font("Impact", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.ForeColor = System.Drawing.Color.OrangeRed;
+            this.lblInfo.Location = new System.Drawing.Point(293, 55);
+            this.lblInfo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(220, 29);
+            this.lblInfo.TabIndex = 3;
+            this.lblInfo.Text = "Welcome To My Lable";
+            this.lblInfo.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ClientSize = new System.Drawing.Size(765, 456);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.txtData);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
+
