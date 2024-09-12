ee0476ab649f5b39c192f4ed59ceefd76174d943
yuval1847 <117625084+yuval1847@users.noreply.github.com>
2024-09-12 14:49:42 +0300

    Delete class_project_1 directory

--- class_project_1/class_project_1/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-public class cube_2d
-{
-    public int x;
-    public int y;
-    public int z;
-}
