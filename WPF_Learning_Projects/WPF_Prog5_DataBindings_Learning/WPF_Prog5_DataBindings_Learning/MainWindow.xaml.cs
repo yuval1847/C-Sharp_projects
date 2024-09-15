74b3f4ca4d23fc675a7bfffbdf2af66bceef598d
yuval1847 <yuval1847@gmail.com>
2024-09-15 20:13:53 +0300

    Add new project

---
+++ WPF_Learning_Projects/WPF_Prog5_DataBindings_Learning/WPF_Prog5_DataBindings_Learning/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Prog5_DataBindings_Learning
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private string sharedtxt;
+        public string SharedTxt
+        { 
+            get { return sharedtxt; }
+            set { sharedtxt = value;
+                  PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SharedTxt"));
+            } 
+        }
+
+        
+    }
+}
