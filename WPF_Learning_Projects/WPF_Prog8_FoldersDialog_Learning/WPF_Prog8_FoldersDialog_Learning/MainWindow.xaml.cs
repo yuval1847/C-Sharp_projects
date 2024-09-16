781e1032cd6460695efbfa8c28db7369f5c23345
yuval1847 <117625084+yuval1847@users.noreply.github.com>
2024-09-16 18:42:29 +0300

    Add files via upload

---
+++ WPF_Learning_Projects/WPF_Prog8_FoldersDialog_Learning/WPF_Prog8_FoldersDialog_Learning/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using WinForms = System.Windows.Forms;
+
+namespace WPF_Prog8_FoldersDialog_Learning
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            WinForms.FolderBrowserDialog foldersDialog = new WinForms.FolderBrowserDialog();
+            WinForms.DialogResult dialogResult = foldersDialog.ShowDialog();
+            if (dialogResult == WinForms.DialogResult.OK)
+            {
+                pathLbl.Text = foldersDialog.SelectedPath;
+            }
+            else
+            {
+                pathLbl.Text = "No folder selected!";
+            }
+        }
+    }
+}
