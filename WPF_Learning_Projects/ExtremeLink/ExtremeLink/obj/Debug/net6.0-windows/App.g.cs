46d63eac0950022e34b006550291e03f9e0d16a3
yuval1847 <yuval1847@gmail.com>
2024-09-18 11:58:20 +0300

    Update

--- WPF_Learning_Projects/ExtremeLink/ExtremeLink/obj/Debug/net6.0-windows/App.g.cs
+++ WPF_Learning_Projects/ExtremeLink/ExtremeLink/obj/Debug/net6.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace ExtremeLink {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace ExtremeLink {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
         public static void Main() {
             ExtremeLink.App app = new ExtremeLink.App();
             app.InitializeComponent();
