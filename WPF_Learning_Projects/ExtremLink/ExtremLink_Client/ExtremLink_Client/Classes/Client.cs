f08788db696791f1f7b8b747e01286bdd97611af
yuval1847 <117625084+yuval1847@users.noreply.github.com>
2024-10-18 17:33:26 +0300

    Continue
    I added get and send function for server and client classes. In addition I created an handle with sql for users login

--- WPF_Learning_Projects/ExtremLink/ExtremLink_Client/ExtremLink_Client/Classes/Client.cs
+++ WPF_Learning_Projects/ExtremLink/ExtremLink_Client/ExtremLink_Client/Classes/Client.cs
@@ -174,5 +174,123 @@ namespace ExtremLink_Client.Classes
             // The function connects the TCP socket to the server.
             this.tcpSocket.Connect(this.serverIpAddr, 1234);
         }
+
+        public void Start()
+        {
+            // The function gets nothing.
+            // The function starts the tasks of the functions which handling with packets.
+            Console.WriteLine("Server started on UDP port 1234 and TCP port 1847.");
+
+            Task.Run(() => this.HandleUdpCommunication());
+            Task.Run(() => this.HandleTcpCommunication());
+        }
+
+        private async Task HandleUdpCommunication()
+        {
+            // The function gets nothing.
+            // The fucntion handle with different type of TCP packets which are sent by the server.
+        }
+
+        private async Task HandleTcpCommunication()
+        {
+            // The function gets nothing.
+            // The fucntion handle with different type of TCP packets which are sent by the server.
+            // The types of message are:
+            // ! - Database functionality
+            // @ -
+            // # - 
+            List<object> message = GetMessage(tcpSocket);
+            string data = (string)message[2];
+            switch (message[0])
+            {
+                case "!":
+                    if(data == "Exist")
+                    {
+
+                    }
+                    if(data == "NotExist")
+                    {
+
+                    }
+                    break;
+            }
+        }
+
+        public byte[] Compress(string data)
+        {
+            // The function gets a string.
+            // The function return the given string after compressing in bytes format.
+            if (string.IsNullOrEmpty(data))
+                return new byte[0];
+
+            byte[] dataBytes = Encoding.UTF8.GetBytes(data);
+
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var gzipStream = new GZipStream(memoryStream, CompressionMode.Compress))
+                {
+                    gzipStream.Write(dataBytes, 0, dataBytes.Length);
+                }
+                return memoryStream.ToArray();
+            }
+        }
+        public string Decompress(byte[] data)
+        {
+            // The function gets a byte array.
+            // The function return the given byte array as a string after decompressing.
+            if (data == null || data.Length == 0)
+                return string.Empty;
+
+            using (var memoryStream = new MemoryStream(data))
+            {
+                using (var gzipStream = new GZipStream(memoryStream, CompressionMode.Decompress))
+                using (var reader = new StreamReader(gzipStream, Encoding.UTF8))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+
+        public void SendMessage(Socket clientSocket, string typeOfMessage, string data)
+        {
+            // The function gets a socket and  2 strings: 'typeOfMessage' which is a symbol which reprsent the type of the message
+            // and 'data' which contains the data which have to be transfered.
+            // The function creates a message in a byte array format and sends it to the client.
+            // The message format: Byte{typeOfMessage(string), dataLength(int), data(string), "EOM"}
+            string endOfMessage = "EOM";
+            string message = $"{typeOfMessage}|{data.Length}|{data}|{endOfMessage}";
+            byte[] compressedMessage = this.Compress(message);
+            clientSocket.Send(compressedMessage);
+        }
+        public List<object> GetMessage(Socket clientSocket)
+        {
+            // The function gets a socket.
+            // The function recieve a message from the socket and returns the message in parts as a list object.
+            byte[] buffer = new byte[4096];
+            int bytesRead = clientSocket.Receive(buffer);
+            string[] messageParts = this.Decompress(buffer).Split('|');
+
+            if (messageParts.Length != 4)
+            {
+                throw new Exception("Error: The message isn't in the right format!");
+            }
+
+            if (messageParts[3] != "EOM")
+            {
+                throw new Exception("End of message not received correctly. The message is cut.");
+            }
+
+            List<object> messagePartsList = new List<object>();
+            for (int i = 0; i < messageParts.Length; i++)
+            {
+                if (i == 1)
+                {
+                    messagePartsList.Add(int.Parse(messageParts[i]));
+                }
+                messagePartsList.Add(messageParts[i]);
+            }
+            return messagePartsList;
+        }
+
     }
 }
--- WPF_Learning_Projects/ExtremLink/ExtremLink_Server/ExtremLink_Server/Classes/Server.cs
+++ WPF_Learning_Projects/ExtremLink/ExtremLink_Server/ExtremLink_Server/Classes/Server.cs
@@ -15,6 +15,7 @@ using System.Net.Http;
 using System.IO;
 using System.IO.Compression;
 using System.ComponentModel;
+using System.Windows.Markup;
 
 
 namespace ExtremLink_Server.Classes
@@ -224,34 +225,146 @@ namespace ExtremLink_Server.Classes
             Task.Run(() => this.HandleTcpCommunication());
         }
 
+
         private async Task HandleUdpCommunication()
         {
             // The function gets nothing.
             // The fucntion handle with different type of TCP packets which are sent by the client.
-
-            while (true)
-            {
-                // Receive UDP data
-                
-
-                // Respond to the UDP client
-                
-            }
         }
 
         private async Task HandleTcpCommunication()
         {
             // The function gets nothing.
             // The fucntion handle with different type of TCP packets which are sent by the client.
-
-            while (true)
+            // The types of message are:
+            // ! - Database functionality
+            // @ -
+            // # - 
+            List<object> message = GetMessage(tcpSocket);
+            string data = (string)message[2];
+            switch (message[0])
             {
-                // Receive TCP data
-                
-
-                // Respond to the TCP client
-                
+                case "!":
+                    if (data.Contains("username"))
+                    {
+                        string username = data.Split(",")[0].Split("=")[1];
+                        string password = data.Split(",")[1].Split("=")[1];
+                        if (this.IsUserExist(username, password, "ExtremLinkDB.mdf"))
+                        {
+                            // Complete if the user exist in the database.
+                            this.SendMessage(tcpSocket, "!", "Exist");
+                        }
+                        else
+                        {
+                            // Complete if the user doesn't exist in the database.
+                            this.SendMessage(tcpSocket, "!", "NotExist");
+                        }
+                    }
+                    break;
             }
+        }
+
+        public byte[] Compress(string data)
+        {
+            // The function gets a string.
+            // The function return the given string after compressing in bytes format.
+            if (string.IsNullOrEmpty(data))
+                return new byte[0];
+
+            byte[] dataBytes = Encoding.UTF8.GetBytes(data);
+
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var gzipStream = new GZipStream(memoryStream, CompressionMode.Compress))
+                {
+                    gzipStream.Write(dataBytes, 0, dataBytes.Length);
+                }
+                return memoryStream.ToArray();
+            }
+        }
+        public string Decompress(byte[] data)
+        {
+            // The function gets a byte array.
+            // The function return the given byte array as a string after decompressing.
+            if (data == null || data.Length == 0)
+                return string.Empty;
+
+            using (var memoryStream = new MemoryStream(data))
+            {
+                using (var gzipStream = new GZipStream(memoryStream, CompressionMode.Decompress))
+                using (var reader = new StreamReader(gzipStream, Encoding.UTF8))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+
+        public void SendMessage(Socket clientSocket, string typeOfMessage, string data)
+        {
+            // The function gets a socket and  2 strings: 'typeOfMessage' which is a symbol which reprsent the type of the message
+            // and 'data' which contains the data which have to be transfered.
+            // The function creates a message in a byte array format and sends it to the client.
+            // The message format: Byte{typeOfMessage(string), dataLength(int), data(string), "EOM"}
+            string endOfMessage = "EOM";
+            string message = $"{typeOfMessage}|{data.Length}|{data}|{endOfMessage}";
+            byte[] compressedMessage = this.Compress(message);
+            clientSocket.Send(compressedMessage);
+        }
+        public List<object> GetMessage(Socket clientSocket)
+        {
+            // The function gets a socket.
+            // The function recieve a message from the socket and returns the message in parts as a list object.
+            byte[] buffer = new byte[4096];
+            int bytesRead = clientSocket.Receive(buffer);
+            string[] messageParts = this.Decompress(buffer).Split('|');
+
+            if (messageParts.Length != 4)
+            {
+                throw new Exception("Error: The message isn't in the right format!");
+            }
+
+            if (messageParts[3] != "EOM")
+            {
+                throw new Exception("End of message not received correctly. The message is cut.");
+            }
+
+            List<object> messagePartsList = new List<object>();
+            for(int i = 0; i < messageParts.Length; i++)
+            {
+                if(i == 1)
+                {
+                    messagePartsList.Add(int.Parse(messageParts[i]));
+                }
+                messagePartsList.Add(messageParts[i]);
+            }
+            return messagePartsList;
+        }
+
+
+        // SQL database queries functions
+        public static SqlConnection ConnectToDB(string fileName)
+        {
+            // The function gets a string which represent the file name of the database.
+            // The fucntion returns a SqlConnection object which connected to the database.
+
+            // The path of the database
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
+            string connString = @"Data Source=(LocalDB)\MSSQLLocalDB; AttachDbFilename=" + path + "; Integrated Security=True; Connect Timeout=30";
+            SqlConnection conn = new SqlConnection(connString);
+            return conn;
+        }
+        public bool IsUserExist(string username, string password, string databaseFileName)
+        {
+            // The function gets 2 strings which represent a username and a password.
+            // The function sends a query to the database and returns true if the user exist, otherwise false.
+            string sqlQuery = $"SELECT * FROM [dbo].[Table] WHERE username='{username}' AND password='{password}'";
+            SqlConnection conn = ConnectToDB(databaseFileName);
+            conn.Open();
+            SqlCommand com = new SqlCommand(sqlQuery, conn);
+            SqlDataReader data = com.ExecuteReader();
+            bool found = (bool)data.Read();
+            conn.Close();
+            return found;
         }
     }
 }
